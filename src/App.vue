fc6ecd169e0d6491e1cab7311d4e8b04cd72eb1a
PierrePocheron <pierre.pocheron@gmail.com>
2023-01-06 11:41:04 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Test</p>
+    <MyButton />
+  </div>
+</template>
+
+<script>
+import MyButton from "@/components/MyButton.vue";
+export default {
+  components: {
+    MyButton,
+  },
+};
+</script>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>My button du components</div>
+</template>
+
+<script>
+export default {};
+</script>

6f8f253d2a4b69bb3fd780bfb46d99ab11b94606
PierrePocheron <pierre.pocheron@gmail.com>
2023-01-06 12:24:28 +0100

    feat: add button components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
-    <p>Test</p>
-    <MyButton />
+    <h1>Title App</h1>
+
+    <p>
+      Theme :
+      <select v-model="theme">
+        <option value="primary">primary</option>
+        <option value="danger">danger</option>
+      </select>
+    </p>
+
+    <p>
+      Size :
+      <select v-model="size">
+        <option value="sm">sm</option>
+        <option value="md">md</option>
+        <option value="lg">lg</option>
+      </select>
+    </p>
+
+    <MyButton theme="theme" size="size" disabled="disabled"
+      >Button du theme</MyButton
+    >
+
+    <MyButton theme="warning" size="sm">Remplace le slot</MyButton>
+    <MyButton theme="warning" size="sm" :disabled="true"
+      >Remplace le slot</MyButton
+    >
+    <MyButton theme="danger" size="lg">Nom bouton 2</MyButton>
+    <MyButton theme="danger" size="lg" disabled block loading
+      >Nom bouton 2</MyButton
+    >
+    <MyButton>Buuton default</MyButton>
   </div>
 </template>
 
@@ -10,6 +40,15 @@ import MyButton from "@/components/MyButton.vue";
 export default {
   components: {
     MyButton,
+  },
+
+  data() {
+    return {
+      theme: "primary",
+      size: "md",
+      disabled: false,
+      block: false,
+    };
   },
 };
 </script>
--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,7 +1,62 @@
 <template>
-  <div>My button du components</div>
+  <button
+    type="button"
+    class="btn"
+    :class="[`btn-${theme}`, size && `btn-${size}`, { 'd-block w-100': block }]"
+    :disabled="disabled"
+  >
+    <div>loading</div>
+    <slot />
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    theme: {
+      type: String,
+      required: false,
+      default: "primary",
+      validator: (value) => {
+        return [
+          "primary",
+          "secondary",
+          "success",
+          "danger",
+          "warning",
+          "info",
+          "light",
+          "dark",
+        ].includes(value);
+      },
+    },
+    size: {
+      type: String,
+      required: false,
+      default: "md",
+      validator: (value) => {
+        return [
+          "sm",
+          "md",
+          "lg",
+          "xl",
+          "2xl",
+          "3xl",
+          "4xl",
+          "5xl",
+          "6xl",
+          "7xl",
+        ];
+      },
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+    },
+    loading: {
+      type: Boolean,
+      required: false,
+    },
+  },
+};
 </script>
